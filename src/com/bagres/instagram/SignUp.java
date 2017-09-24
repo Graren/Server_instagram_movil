8942742b69908766119d705a792ec003e36511d7
Oreste <oresteh97@hotmail.com>
2017-09-24 16:14:23 -0400

    Sign Up, la cague en el anterior xd

--- src/com/bagres/instagram/SignUp.java
+++ src/com/bagres/instagram/SignUp.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SignUp
  */
-@WebServlet("/SignUp")
+@WebServlet("/signup")
 public class SignUp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -35,7 +35,36 @@ public class SignUp extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setHeader("Content-Type","application/json");
+		String user_id = request.getParameter("user_id");
+		String user_name = request.getParameter("user_name");
+		String user_password = request.getParameter("user_password");
+		String user_fullname = request.getParameter("user_fullname");
+		String dateString = request.getParameter("user_datebirth");
+		boolean user_gender= Boolean.parseBoolean(request.getParameter("user_gender"));
+		String user_description = request.getParameter("user_description");
+		Integer id = Integer.parseInt(user_id);
+		Date user_datebirth = null;
+		try {
+			user_datebirth = new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
+		}
+		catch(ParseException e) {
+			e.printStackTrace();
+		}
+		Gson js = new Gson();
+		try {
+			String query = "INSERT INTO user (user_id,user_name,user_password,user_fullname,user_datebirth,user_gender,user_description) VALUES(?,?,?,?,?,?,?)";
+			Object[] values = {user_id,user_name,user_password,user_fullname,user_datebirth,user_gender,user_description};
+			JDBCon jdbc = new JDBCon();
+			jdbc.doConnect(5432,"localhost","instagramdb","postgres","masterkey");
+			jdbc.execute(query,values);
+			js.add("status",200).add("message","The User has been created");
+		}
+		catch(Exception e1) {
+			e1.printStackTrace();
+			js.add("status",404).add("Message","Error");
+		}
+		System.out.print(js);
+		response.getWriter().print(js);
 	}
-
 }
